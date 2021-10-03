d27baf7e0d4dff857dd6fd3303390cc9c30beeb4
Максим Секачёв <cement-fan@ya.ru>
2021-10-03 16:24:58 +0300

    swagger url

--- statistic_counter/urls.py
+++ statistic_counter/urls.py
@@ -25,20 +25,20 @@ schema_view = get_schema_view(
         title="Statistic",
         default_version='v1',
         description="Документация для проекта statistics_counter",
-        contact=openapi.Contact(email="admin@kittygram.ru"),
-        license=openapi.License(name="BSD License"),
     ),
     public=True,
     permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
     url(r'^swagger(?P<format>\.json|\.yaml)$',
         schema_view.without_ui(cache_timeout=0), name='schema-json'),
     url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
         name='schema-swagger-ui'),
     url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
         name='schema-redoc'),
+
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
+
 ]
